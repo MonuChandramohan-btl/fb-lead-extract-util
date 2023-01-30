7fca643a267fbd48242e680ecc4c1ab419ef252e
MonuChandramohan <monu.chandramohan@blueteaklabs.com>
2023-01-30 12:49:58 +0530

    jan30 changes

--- src/main/java/com/blueteak/fbleads/extraction/FbLeadsExtract.java
+++ src/main/java/com/blueteak/fbleads/extraction/FbLeadsExtract.java
@@ -38,26 +38,26 @@ public class FbLeadsExtract {
 		System.out.println("################### File Read done ######################");
 		
 //		// check if existing in DB
-		fbLeadReqList = fbLeadsExtract.fbLeadsJDBC.isFBLeadExists(fbLeadReqList);
-		System.out.println("################### JDBC Check done ######################");
-		
+//		fbLeadReqList = fbLeadsExtract.fbLeadsJDBC.isFBLeadExists(fbLeadReqList);
+//		System.out.println("################### JDBC Check done ######################");
+//		
 		CSVWriter csvWriter = new CSVWriter(fbLeadReqList);
 		String genCSVFileName = FbExtractConstants.COMBINED_CSV_LOCATION + "/" + FbExtractConstants.COMBINED_CSV_FILENAME + java.time.LocalDate.now() + "-"
 				+ fbLeadsExtract.fbLeadsHelper.getRandomNumberString() + ".csv";
 		csvWriter.writeCSV(genCSVFileName);
 	
 		
-		// generate log file
-		String genFileName = FbExtractConstants.EXTRACTION_LOCATION + "/" + FbExtractConstants.EXTRACTED_FILENAME + java.time.LocalDate.now() + "-"
-				+ fbLeadsExtract.fbLeadsHelper.getRandomNumberString() + ".txt";
-		fbLeadsExtract.fbResponseProcessor.processResponse(fbLeadReqList, genFileName);
-		System.out.println("################### Process Response done ######################");
-		
-		// generate log file
-		String updateScriptFileName = FbExtractConstants.EXTRACTION_CREATED_DATE_LOCATION + "/" + FbExtractConstants.EXTRACTED_CREATED_DATE_FILENAME + java.time.LocalDate.now() + "-"
-						+ fbLeadsExtract.fbLeadsHelper.getRandomNumberString() + ".sql";
-		fbLeadsExtract.fbUpdateScriptProcessResponse.processResponse(fbLeadReqList, updateScriptFileName);
-		System.out.println("################### SQL :: Process  Response done ######################");
+//		// generate log file
+//		String genFileName = FbExtractConstants.EXTRACTION_LOCATION + "/" + FbExtractConstants.EXTRACTED_FILENAME + java.time.LocalDate.now() + "-"
+//				+ fbLeadsExtract.fbLeadsHelper.getRandomNumberString() + ".txt";
+//		fbLeadsExtract.fbResponseProcessor.processResponse(fbLeadReqList, genFileName);
+//		System.out.println("################### Process Response done ######################");
+//		
+//		// generate log file
+//		String updateScriptFileName = FbExtractConstants.EXTRACTION_CREATED_DATE_LOCATION + "/" + FbExtractConstants.EXTRACTED_CREATED_DATE_FILENAME + java.time.LocalDate.now() + "-"
+//						+ fbLeadsExtract.fbLeadsHelper.getRandomNumberString() + ".sql";
+//		fbLeadsExtract.fbUpdateScriptProcessResponse.processResponse(fbLeadReqList, updateScriptFileName);
+//		System.out.println("################### SQL :: Process  Response done ######################");
 	}
 	
 }
